6cc6a941e60cc04b598d1c52cea4515e9b3df3a9
Arjun Ajithan <112150483+GundaBoii@users.noreply.github.com>
2024-04-13 02:35:31 +0530

    Update members.php [tab Picture Added]

--- members.php
+++ members.php
@@ -49,6 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Member Management</title>
+    <link rel="icon" href="home.png" type="image/x-icon" />
     <link rel="stylesheet" href="memberstyle.css">
 </head>
 
